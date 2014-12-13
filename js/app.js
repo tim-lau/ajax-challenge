aab796c6b7cbb271ac6c6bd0b2d9ecf763cdfba1
Tim <timothyl@uw.edu>
2014-12-12 19:22:33 -0800

    comments push to parse, update in list, and delete

--- js/app.js
+++ js/app.js
@@ -1,5 +1,64 @@
 "use strict";
-/*
-    app.js, main Angular application script
-    define your module and controllers here
-*/
+
+var responseUrl = 'https://api.parse.com/1/classes/responses';
+
+angular.module('ResponseApp', ['ui.bootstrap'])
+    .config(function($httpProvider) {
+        $httpProvider.defaults.headers.common['X-Parse-Application-Id'] = 'ZqCzpwBC7JrhOrjXYimDe8Xq7Ict6zQo8aNC9KaO';
+        $httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = 'hhE8faMyCnmhop21OjJdhJ2V9Zfx3s8YU7xUzNY9';
+    })
+
+    .controller('ResponseController', function($scope, $http) {
+        $scope.newComment = {
+            score: 0,
+        };
+
+        // Submits a new comment to Parse
+        $scope.addComment = function () {
+            $scope.loading = true;
+            $http.post(responseUrl, $scope.newComment)
+                .success(function (responseData) {
+                    $scope.newComment.objectId = responseData.objectId;
+                    $scope.responses.push($scope.newComment);
+                })
+                .error(function (err) {
+                    $scope.errorMessage = err;
+                    console.log(err);
+                })
+                .finally(function() {
+                    $scope.newComment = {
+                        score: 0,
+                    };
+                    $scope.loading = false;
+                    $scope.updateComments();
+                });
+        };
+
+        // Updates comment list via Parse
+        $scope.updateComments = function () {
+            $scope.loading = true;
+            $http.get(responseUrl + "?order=-score")
+                .success(function (data) {
+                    $scope.comments = data.results;
+                })
+                .error(function (err) {
+                    $scope.errorMessage = err;
+                    console.log(err);
+                })
+                .finally(function () {
+                	$scope.form.$setPristine();
+                    $scope.loading = false;
+                });
+        };
+
+        $scope.updateComments();
+
+        // Removes comment from comment list via Parse
+        $scope.removeComment = function(comment) {
+            $http.delete(responseUrl + '/' + comment.objectId, comment)
+                .finally(function() {
+                    $scope.updateComments();
+                });
+        };
+
+    });
